0119827430ee924d5c4461c7be206f9456e53731
CC-River <midhunraaj9b@gmail.com>
2026-03-20 17:17:58 +0530

    Added Level 1 Task Part B

--- rovercoords.cpp
+++ rovercoords.cpp
@@ -5,15 +5,32 @@ using namespace std;
 float distance(float x1, float y1, float x2, float y2){
     float x = pow((x2-x1),2);
     float y = pow((y2-y1),2);
-    float dist = sqrt(x+y);
-    return dist;
+    return sqrt(x+y);
+}
+
+float time(float dist, float u, float a, float v){
+    float t1 = (v-u)/a;
+    float s1 = u*t1 + 0.5*a*t1*t1;
+    if (s1>=dist) {
+        float t = (-u+sqrt(u*u+2*a*dist))/a;
+        return t;
+    }
+    float s2 = dist-s1;
+    float t2 = s2/v;
+    return t1 + t2;
 }
 
 int main(){
-    float a,b,c,d;
+    float a,b,c,d, vel, accl, speed;
     cout<<"Enter x1 y1 and x2 y2: ";
     cin>>a>>b>>c>>d;
-    float q = distance(a,b,c,d);
-    cout<<"The distance is: "<<q;
+    float dist = distance(a,b,c,d);
+    cout<<"The distance is: "<<dist<<endl;
+
+    cout<<"Enter initial velocity, acceleration and max speed: ";
+    cin>>vel>>accl>>speed;
+    float t = time(dist, vel, accl, speed);
+    cout << "Time to reach destination: " <<t<<"s"<< endl;
+
     return 0;
 }
